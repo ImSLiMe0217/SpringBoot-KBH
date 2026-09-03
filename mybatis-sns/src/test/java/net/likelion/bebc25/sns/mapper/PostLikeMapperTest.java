3c0094d22606c32ed8da5f826c5c64e93eb2b193
Byeongho Kim <qudgh0217@naver.com>
2026-09-03 11:50:25 +0900

    좋아요 관련 기능 구현 및 테스트 준비

---
+++ mybatis-sns/src/test/java/net/likelion/bebc25/sns/mapper/PostLikeMapperTest.java
@@ -0,0 +1,10 @@
+package net.likelion.bebc25.sns.mapper;
+
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PostLikeMapperTest {
+
+}
